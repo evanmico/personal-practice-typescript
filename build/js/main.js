13f7dfe5c44323519fb4f7cd35a315138ca5368a
Ivan Rosyaykin <evanmico222@gmail.com>
2023-05-05 01:13:35 -0400

    TypeScriptTypeSpecificationPractice_Lesson2

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,17 @@
 "use strict";
-let username = 'ovcchchh';
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+let myName = "Ivan";
+let definitionOfLife;
+let isLoading;
+let album;
+definitionOfLife = 15;
+isLoading = true;
+album = 2222; //can be any datatype, try not to use this too much as it defeats ts purpose
+album = "The Black Album";
+const sum = (a, b) => {
+    return a + b;
+};
+//Realistic Cases Where You May Use Union Type
+let postId;
+let isActive;
+let re = /\w+/g; //If not sure what type something is, mouse over and see what typescript infers
+let reWithSpecifiedType = /\w+/g; // the result
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,20 @@
-let username = 'ovcchchh';
-console.log(username);
+let myName: string = "Ivan";
+let definitionOfLife: number;
+let isLoading: boolean;
+let album: any;
 
-let a: number = 12;
-let b: number = 6;
-let c: number = 2;
+definitionOfLife = 15;
+isLoading = true;
+album = 2222; //can be any datatype, try not to use this too much as it defeats ts purpose
+album = "The Black Album";
 
-console.log(a / b);
+const sum = (a: number, b: string) => {
+    return a + b;
+}
 
-console.log(c * b);
+//Realistic Cases Where You May Use Union Type
+let postId: string | number;
+let isActive: number | boolean;
+
+let re = /\w+/g; //If not sure what type something is, mouse over and see what typescript infers
+let reWithSpecifiedType:RegExp = /\w+/g; // the result
