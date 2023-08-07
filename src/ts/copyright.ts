2d0fe03165931f19ae947367172a9ff0ffb8893c
Ivan Rosyaykin <evanmico222@gmail.com>
2023-08-07 01:33:03 -0400

    practicalApplicationTSExample

---
+++ src/ts/copyright.ts
@@ -0,0 +1,31 @@
+//OG JS Code
+// const year = document.getElementById('year');
+// const thisYear = new Date().getFullYear();
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
+
+//My TS Solution
+// const year = document.getElementById('year')!;
+// const thisYear: number = new Date().getFullYear();
+
+// year.setAttribute("datetime", '' + thisYear);
+// year.textContent ="" + thisYear;
+
+
+// 1st Variation
+// let year: HTMLElement | null
+// year = document.getElementById('year');
+// let thisYear: string
+// thisYear = new Date().getFullYear().toString();
+// if(year){ //typeguard
+//     year.setAttribute("datetime", thisYear);
+//     year.textContent = thisYear;
+// }
+
+// 2nd Variation
+const year = document.getElementById('year') as HTMLSpanElement;
+const thisYear: string = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
+
+
